2372bda2f1ed88d76a6a4a2167043deb77f30cd5
Killian Kim <killianjyk@gmail.com>
2023-03-06 22:35:14 -0600

    Feature 13 complete

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
+from projects.forms import ProjectForm
 
 # Create your views here.
 
@@ -12,3 +13,28 @@ def list_projects(request):
         "projects": projects,
     }
     return render(request, "projects/list.html", context)
+
+
+@login_required
+def show_project(request, id):
+    list = Project.objects.get(id=id)
+    context = {
+        "list": list,
+    }
+    return render(request, "projects/detail.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list_projects")
+    else:
+        form = ProjectForm()
+
+    context = {
+        "form": form,
+    }
+    return render(request, "projects/create.html", context)
